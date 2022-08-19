647cacf5731dd8683fd4631cc4e113ec31eb108a
Tosin Ayodele <devtosxn@gmail.com>
2022-08-19 21:35:30 +0100

    deploy contract and mint NFT script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,22 @@
 import { ethers } from "hardhat";
 
-// NFT Contract Address: 0xf033474E9d0c4C01a330b1cD8605FAf25fEA99e4
+// NFT Contract Address: 0xa4B4776E716dC7C84DCEaeD3267976B0aa98494C
+// NFT LINK: https://testnets.opensea.io/assets/rinkeby/0xa4b4776e716dc7c84dceaed3267976b0aa98494c/0
+
 const main = async () => {
   const ToxNFTFactory = await ethers.getContractFactory("ToxNFT");
   const toxnft = await ToxNFTFactory.deploy();
 
   await toxnft.deployed();
 
+  const address = "0x4e9776985f2780c3c99Ff2D69C9AebD583F00ECE"
+  const ipfsCID = "ipfs://QmNt3DCHtm2y3JtcXop6Y6q4CiM2WPvzci2vLTtPk8ZXAW"
+
+  await toxnft.safeMint(address, ipfsCID);
+  const balance = await toxnft.balanceOf(address);
+  
   console.log("ToxToken Contract deployed to:", toxnft.address);
+  console.log("Minted Address Balance: ", balance);
 }
 
 main().catch((error) => {
