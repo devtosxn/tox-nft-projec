a278c8e79f57da1c44cbaafd3b8a4a2fcc51ff14
Tosin Ayodele <devtosxn@gmail.com>
2022-08-19 19:51:27 +0100

    deploy NFT contract to testnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,6 @@
 import { ethers } from "hardhat";
 
+// NFT Contract Address: 0xf033474E9d0c4C01a330b1cD8605FAf25fEA99e4
 const main = async () => {
   const ToxNFTFactory = await ethers.getContractFactory("ToxNFT");
   const toxnft = await ToxNFTFactory.deploy();
